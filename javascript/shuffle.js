a6d3d0ee6e3ec3d57f4ec3443a9b2761243d9c82
username83621 <mr.dalv94@gmail.com>
2025-11-13 23:21:57 +0200

    Added shuffle.js

---
+++ javascript/shuffle.js
@@ -0,0 +1,19 @@
+
+// https://bigfrontend.dev/problem/can-you-shuffle-an-array
+
+
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]]
+  }
+
+  return arr
+}
+
+
+console.log(shuffle([1, 2, 3, 4, 5]))
+console.log(shuffle([1, 2, 3, 4, 5]))
+console.log(shuffle([1, 2, 3, 4, 5]))
+console.log(shuffle([1, 2, 3, 4, 5]))
+console.log(shuffle([1, 2, 3, 4, 5]))
